246b2e4b5975bdbe7b15901c1f7f16dfb7500b67
Adityan Nampoothiry <165512407+adtyn-0@users.noreply.github.com>
2025-07-12 17:53:31 +0530

    Update init.lua for nvimtree

--- configs/nvim/lua/adtyn/init.lua
+++ configs/nvim/lua/adtyn/init.lua
@@ -3,3 +3,9 @@ require("adtyn.lazy_init")
 require("adtyn.remap")
 require("adtyn.set")
 
+-- disable netrw at the very start of your init.lua
+ vim.g.loaded_netrw = 1
+ vim.g.loaded_netrwPlugin = 1
+--
+ -- optionally enable 24-bit colour
+ vim.opt.termguicolors = true
