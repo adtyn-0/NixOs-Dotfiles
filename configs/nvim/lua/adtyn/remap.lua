b7fb46d51fda76b9d41a87c1313d251afaf2c7d9
adtyn-0 <adityan.nampoothiry1345@gmail.com>
2026-04-02 02:09:12 +0530

    Finally took out the training wheels

--- configs/nvim/lua/adtyn/remap.lua
+++ configs/nvim/lua/adtyn/remap.lua
@@ -1,17 +1,19 @@
 vim.keymap.set("n", "<leader>lz", ":Lazy<CR>", { desc = "Open Lazy.nvim" })
 
---Clipboard keybindings
-vim.keymap.set({ "n", "v" }, "<leader>c", '"+y', { desc = "Copy to clipboard" })
-vim.keymap.set("n", "<leader>C", '"+Y', { desc = "Copy line to clipboard" })
-vim.keymap.set("n", "<leader>p", '"+p', { desc = "Paste from clipboard" })
-vim.keymap.set("i", "<leader>p", '<C-r>+', { desc = "Paste in insert mode" })
+--Clipboard keybindings [Training wheels]
+-- vim.keymap.set({ "n", "v" }, "<leader>c", '"+y', { desc = "Copy to clipboard" })
+-- vim.keymap.set("n", "<leader>C", '"+Y', { desc = "Copy line to clipboard" })
+-- vim.keymap.set("n", "<leader>p", '"+p', { desc = "Paste from clipboard" })
+-- vim.keymap.set("i", "<leader>p", '<C-r>+', { desc = "Paste in insert mode" })
+
+-- Actually goated
 vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Safe paste in visual mode" })
 
 vim.keymap.set("n", "<leader>e", ":NvimTreeOpen<CR>" , {desc = "Go to Explorer" })
 
 -- Keyboard shortcuts
 vim.keymap.set("i", "<C-H>", "<C-w>", { noremap = true, desc = "Ctrl+Backspace to delete word" })
-vim.keymap.set("n", "<C-a>","ggVG",{ noremap = true, desc = "Select All"})
+-- vim.keymap.set("n", "<C-a>","ggVG",{ noremap = true, desc = "Select All"})
 -- vim.keymap.set()
 
 -- Clear all yank registers and system clipboard
